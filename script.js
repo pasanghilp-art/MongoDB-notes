ce82fb963a18f8e833aaf6f3ff4b67fa460fcaff
Pasang Tamang <pasanghilp@gmail.com>
2026-09-17 10:47:26 +0545

    Add schema Types

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const User = require("./user");
 
 mongoose.connect("mongodb://localhost/usersdb");
+
+run()
+async function run(){
+    try {
+         const user = await User.create({ 
+                name: "Pasang",
+                age: 18,
+                hobbies: ["Gym, drawing, Walking"],
+                address: {
+                    street: "Main st"
+                },
+             })
+             console.log(user)
+        } catch (e) {
+        console.log(e.message)
+    }
+}
